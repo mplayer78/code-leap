2a5a8ef1c757a16b61e822b8f9203effc8136b2a
mattplayer <mpl@bookboon.com>
2022-08-31 16:59:48 +0100

    remove history

--- .history/src/challenges/intro_20220828085202.ts
+++
@@ -1,7 +0,0 @@
-export default function intro = () => ({
-    "heading": "Code Leap",
-    "description": `Theorizing that one can time travel within his own lifetime, Dr. Sam Beckett stepped into the Quantum Accelerator, and vanished.
-He awoke to find himself trapped in the past, facing mirror images that were not his own; and driven by an unknown force to change history for the better.
-His only guide in this journey is Al, an observer from his own time, who appears in the form of a hologram that only Sam can see and here.
-And so Dr. Beckett finds himself, leaping from life to life, striving to put right what once went wrong, and hoping each time that his next leap, will be the leap home...`
-})
